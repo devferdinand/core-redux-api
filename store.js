effb779119fad4f097e54f0f84ffe9c360ea60c1
Ferdinand Lukito <devferdinandlukito@gmail.com>
2024-02-27 15:40:13 -0500

    third commit
    Implemented action creators for loadData(), addRecipe(), and removeRecipe() in store.js

--- store.js
+++ store.js
@@ -1,3 +1,5 @@
+import { allRecipesData } from './data';
+
 const initialState = {
     allRecipes: [],
     favoriteRecipes: [],
@@ -11,31 +13,40 @@ export const setSearchTerm = (term) => {
       type: 'searchTerm/setSearchTerm', 
       payload: term 
     };
-  }
-  
-  // Dispatched when the user presses the clear search button.
-  export const clearSearchTerm = () => {
+};
+
+// Dispatched when the user presses the clear search button.
+export const clearSearchTerm = () => {
     return { 
-      type: 'searchTerm/clearSearchTerm' 
+        type: 'searchTerm/clearSearchTerm' 
     };
-  }
-  
-  // Dispatched when the user first opens the application.
-  // Sends the allRecipesData array to the store.
-  export const loadData = () => {
-    
-  }
-  
-  // Dispatched when the user clicks on the heart icon of 
-  // a recipe in the "All Recipes" section.
-  // Sends the recipe object to the store.
-  export const addRecipe = (recipe) => {
-    
-  }
-  
-  // Dispatched when the user clicks on the broken heart 
-  // icon of a recipe in the "Favorite Recipes" section.
-  // Sends the recipe object to the store.
-  export const removeRecipe = (recipe) => {
-    
-  }
+};
+
+// Dispatched when the user first opens the application.
+// Sends the allRecipesData array to the store.
+export const loadData = () => {
+    return {
+        type: 'allRecipes/loadData',
+        payload: allRecipesData
+    };
+};
+
+// Dispatched when the user clicks on the heart icon of 
+// a recipe in the "All Recipes" section.
+// Sends the recipe object to the store.
+export const addRecipe = (recipe) => {
+    return {
+        type: 'favoriteRecipes/addRecipe',
+        payload: recipe
+    };
+};
+
+// Dispatched when the user clicks on the broken heart 
+// icon of a recipe in the "Favorite Recipes" section.
+// Sends the recipe object to the store.
+export const removeRecipe = (recipe) => {
+    return {
+        type: 'favoriteRecipes/removeRecipe',
+        payload: recipe
+    };
+};
